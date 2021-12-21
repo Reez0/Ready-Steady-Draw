0af1fbe40b53f74dab5ddea3ea550a033db19a38
reez0 <riyaazfakier2@gmail.com>
2021-12-21 22:22:19 +0200

    Removed some comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,14 +21,12 @@ public class GameManager : MonoBehaviour
             break;
         }
     }
-    // Start is called before the first frame update
     void Start()
     {
        randomNumber = Random.Range(1,10);
        _ui = gameObject.GetComponent<UI>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (!roundEnd) {
